feaec47089b191ba341d89c66d4c6e744421e718
daylon <daylonfellipe3001@outlook.com>
2025-05-31 13:33:07 +0100

    ADD - Add Bicycle Entity and Enums

---
+++ Source/Backend/Daylon.BicycleStore.Rent.Domain/Entity/Enum/ColorEnum.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Daylon.BicycleStore.Rent.Domain.Entity.Enum
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum ColorEnum
+    {
+        Indefinite = 0,
+        Black = 1,
+        White = 2,
+        Gray = 3,
+        Red = 4,
+        Blue = 5,
+        Green = 6,
+        Yellow = 7,
+        Orange = 8,
+        Purple = 9
+    }
+}
