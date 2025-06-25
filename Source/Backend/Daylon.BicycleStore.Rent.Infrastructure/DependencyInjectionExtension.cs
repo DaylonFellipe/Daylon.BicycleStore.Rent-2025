df5ab39652262c448205fdb8a952e1c2532ca58a
daylon <daylonfellipe3001@outlook.com>
2025-06-25 19:02:56 +0100

    ADD - Add DbContext

--- Source/Backend/Daylon.BicycleStore.Rent.Infrastructure/DependencyInjectionExtension.cs
+++ Source/Backend/Daylon.BicycleStore.Rent.Infrastructure/DependencyInjectionExtension.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Daylon.BicycleStore.Rent.Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Daylon.BicycleStore.Rent.Infrastructure
 {
     public static class DependencyInjectionExtension
     {
-        public static void AddInfrastructureServices(this IServiceCollection services)
+        public static void AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
+            AddDbContext(services, configuration);
+        }
 
+        private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("SqlServerConnection");
+
+            if (string.IsNullOrEmpty(connectionString))
+            { throw new Exception("Connection string is not configured."); }
+
+            services.AddDbContext<BicycleStoreDbContext>(options =>
+                options.UseSqlServer(connectionString));
         }
     }
 }
