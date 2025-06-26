85c237cf538118bb25a9166c086e5a055ae7d02a
daylon <daylonfellipe3001@outlook.com>
2025-06-26 21:13:55 +0100

    ADD - Add Repositories

---
+++ Source/Backend/Daylon.BicycleStore.Rent.Domain/Repositories/IBicycleRepository.cs
@@ -0,0 +1,9 @@
+﻿using Daylon.BicycleStore.Rent.Domain.Entity;
+
+namespace Daylon.BicycleStore.Rent.Domain.Repositories
+{
+    public interface IBicycleRepository
+    {
+        public Task<IList<Bicycle>> GetBicyclesAsync();
+    }
+}
---
+++ Source/Backend/Daylon.BicycleStore.Rent.Infrastructure/DataAccess/Repositories/BicycleRepository.cs
@@ -0,0 +1,18 @@
+﻿using Daylon.BicycleStore.Rent.Domain.Entity;
+using Daylon.BicycleStore.Rent.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Daylon.BicycleStore.Rent.Infrastructure.DataAccess.Repositories
+{
+    public class BicycleRepository : IBicycleRepository
+    {
+        private readonly BicycleStoreDbContext _dbContext;
+
+        public BicycleRepository(BicycleStoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IList<Bicycle>> GetBicyclesAsync() => await _dbContext.Bicycles.ToListAsync();
+    }
+}
