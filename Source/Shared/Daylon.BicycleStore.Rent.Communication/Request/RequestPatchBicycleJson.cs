c4678db531092f1f4ddec8ffbdc31ded79b37e7a
daylon <daylonfellipe3001@outlook.com>
2025-07-09 18:18:52 +0100

    WIP - Added Patch in Controller

---
+++ Source/Shared/Daylon.BicycleStore.Rent.Communication/Request/RequestPatchBicycleJson.cs
@@ -0,0 +1,20 @@
+﻿using Daylon.BicycleStore.Rent.Domain.Entity.Enum;
+
+namespace Daylon.BicycleStore.Rent.Communication.Request
+{
+    public class RequestPatchBicycleJson
+    {
+        // Basic Properties
+        public string? Name { get; set; } 
+        public string? Description { get; set; } 
+
+        // Technical Properties
+        public BrandEnum? Brand { get; set; }
+        public ModelEnum? Model { get; set; }
+        public ColorEnum? Color { get; set; }
+
+        // Stock Properties
+        public double? Price { get; set; }
+        public int? Quantity { get; set; }
+    }
+}
