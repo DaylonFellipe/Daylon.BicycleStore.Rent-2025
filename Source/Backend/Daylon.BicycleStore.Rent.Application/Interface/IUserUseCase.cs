2612e62731024753da668a11c667b660f364d548
daylon <daylonfellipe3001@outlook.com>
2025-09-03 21:22:42 +0100

    ADD - Added Update Email On Controller and his Logic

--- Source/Backend/Daylon.BicycleStore.Rent.Application/Interface/IUserUseCase.cs
+++ Source/Backend/Daylon.BicycleStore.Rent.Application/Interface/IUserUseCase.cs
@@ -7,6 +7,7 @@
 
         // PATCH
         public Task<Domain.Entity.User> ExecuteUpdateUserNameAsync(Guid id, string? firstName, string? LastName);
+        public Task<Domain.Entity.User> ExecuteUpdateUserEmailAsync(Guid id, string newEmail, string password);
         public Task<Domain.Entity.User> ExecuteUpdateUserPasswordAsync(Guid id, string oldPassword, string newPassword);
 
 
---
+++ Source/Shared/Daylon.BicycleStore.Rent.Communication/Request/User/RequestUpdateUserEmailJson.cs
@@ -0,0 +1,9 @@
+﻿namespace Daylon.BicycleStore.Rent.Communication.Request.User
+{
+    public class RequestUpdateUserEmailJson
+    {
+        public Guid Id { get; set; }
+        public string NewEmail { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
