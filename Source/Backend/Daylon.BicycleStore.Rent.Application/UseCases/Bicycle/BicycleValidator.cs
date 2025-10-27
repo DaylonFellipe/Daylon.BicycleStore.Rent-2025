9acdfa4f45edde20060ee653079a221cd06ef6dd
daylon <daylonfellipe3001@outlook.com>
2025-10-27 15:40:22 -0300

    UPD - Updated Custom UpdateBicycleValidator Exception

--- Source/Backend/Daylon.BicycleStore.Rent.Application/UseCases/Bicycle/BicycleValidator.cs
+++ Source/Backend/Daylon.BicycleStore.Rent.Application/UseCases/Bicycle/BicycleValidator.cs
@@ -51,33 +51,33 @@ namespace Daylon.BicycleStore.Rent.Application.UseCases.Bicycle
             ClassLevelCascadeMode = CascadeMode.Stop;
 
             RuleFor(n => n.Name)
-                .NotEmpty().WithMessage("Bicycle name is required.")
-                .MaximumLength(100).WithMessage("Bicycle name must not exceed 100 characters.");
+                .NotEmpty().WithMessage(ResourceMessagesException.BICYCLE_NAME_EMPTY)
+                .MaximumLength(100).WithMessage(ResourceMessagesException.BICYCLE_NAME_MAX_LENGTH);
 
             RuleFor(d => d.Description)
                 .NotEmpty().WithMessage("Bicycle name is required.")
                 .MaximumLength(500).WithMessage("Bicycle name must not exceed 500 characters.");
 
             RuleFor(m => m.Model)
-                .IsInEnum().WithMessage("Valid model is required")
-                .Must(model => model is not null && Enum.IsDefined(typeof(ModelEnum), model)).WithMessage("Model must be a valid enum value.");
+                .IsInEnum().WithMessage(ResourceMessagesException.BICYCLE_MODEL_REQUIRED)
+                .Must(model => model is not null && Enum.IsDefined(typeof(ModelEnum), model)).WithMessage(ResourceMessagesException.BICYCLE_MODEL_INVALID_ENUM);
 
             RuleFor(b => b.Brand)
-                .IsInEnum().WithMessage("Valid brand is required")
-                .Must(brand => brand is not null && Enum.IsDefined(typeof(BrandEnum), brand)).WithMessage("Brand must be a valid enum value.");
+                .IsInEnum().WithMessage(ResourceMessagesException.BICYCLE_BRAND_REQUIRED)
+                .Must(brand => brand is not null && Enum.IsDefined(typeof(BrandEnum), brand)).WithMessage(ResourceMessagesException.BICYCLE_MODEL_INVALID_ENUM);
 
             RuleFor(c => c.Color)
-                .IsInEnum().WithMessage("Valid color is required")
-                .Must(color => color is not null && Enum.IsDefined(typeof(ColorEnum), color)).WithMessage("Color must be a valid enum value.");
+                .IsInEnum().WithMessage(ResourceMessagesException.BICYCLE_COLOR_REQUIRED)
+                .Must(color => color is not null && Enum.IsDefined(typeof(ColorEnum), color)).WithMessage(ResourceMessagesException.BICYCLE_COLOR_INVALID_ENUM);
 
             RuleFor(p => p.Price)
-                .GreaterThan(0).WithMessage("Price must be greater than zero.");
+                .GreaterThan(0).WithMessage(ResourceMessagesException.BICYCLE_PRICE_GREATER_THAN_ZERO);
 
             RuleFor(q => q.Quantity)
-                .GreaterThanOrEqualTo(0).WithMessage("Quantity must be zero or greater.");
+                .GreaterThanOrEqualTo(0).WithMessage(ResourceMessagesException.BICYCLE_QUANTITY_ZERO_OR_GREATER);
 
             RuleFor(d => d.DailyRate)
-                .GreaterThan(0).WithMessage("Daily rate must be greater than zero.");      
+                .GreaterThan(0).WithMessage(ResourceMessagesException.BICYCLE_DAILY_RATE_GREATER_THAN_ZERO);      
         }
     }
 
