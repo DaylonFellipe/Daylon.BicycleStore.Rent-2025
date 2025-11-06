11d1c7a1126b52827f445f9cfab7cc8129b7cfac
daylon <daylonfellipe3001@outlook.com>
2025-11-06 18:43:04 +0000

    ADD - Added UpdateUserEmailValidator Tests

---
+++ Tests/CommonTestUtilities/Requests/User/RequestUpdateUserEmailJsonBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Bogus;
+using Daylon.BicycleStore.Rent.Communication.Request.User;
+
+namespace CommonTestUtilities.Requests.User
+{
+    public class RequestUpdateUserEmailJsonBuilder
+    {
+        public static RequestUpdateUserEmailJson Build(int passwordLength = 10)
+        {
+            return new Faker<RequestUpdateUserEmailJson>()
+                .RuleFor(u => u.Id, f => Guid.NewGuid())
+                .RuleFor(u => u.NewEmail, f => f.Internet.Email())
+                .RuleFor(u => u.Password, f => f.Internet.Password(passwordLength, true, null, "@1Aa"))
+                .Generate();
+        }
+    }
+}
