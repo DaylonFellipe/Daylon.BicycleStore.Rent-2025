d6b8a06c8d0a81a9751e2ab2e7462c9396d677f2
daylon <daylonfellipe3001@outlook.com>
2025-11-08 23:54:42 +0000

    ADD - Added ModifyDatesValidator Tests

--- Source/Backend/Daylon.BicycleStore.Rent.Application/UseCases/Bicycle/RentalOrderValidator.cs
+++ Source/Backend/Daylon.BicycleStore.Rent.Application/UseCases/Bicycle/RentalOrderValidator.cs
@@ -1,4 +1,4 @@
-﻿    using Daylon.BicycleStore.Rent.Communication.Request;
+﻿using Daylon.BicycleStore.Rent.Communication.Request;
 using Daylon.BicycleStore.Rent.Domain.Entity.Enum;
 using Daylon.BicycleStore.Rent.Exceptions;
 using FluentValidation;
---
+++ Tests/CommonTestUtilities/Requests/RentalOrder/RequestModifyDatesValidatorJsonBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Bogus;
+using Daylon.BicycleStore.Rent.Communication.Request;
+
+namespace CommonTestUtilities.Requests.RentalOrder
+{
+    public class RequestModifyDatesValidatorJsonBuilder
+    {
+        public static RequestModifyDatesValidatorJson Build()
+        {
+            return new Faker<RequestModifyDatesValidatorJson>()
+              .RuleFor(r => r.RentalStart, f => f.Date.Future())
+              .RuleFor(r => r.RentalDays, f => f.Random.Int(1, 90))
+              .RuleFor(r => r.ExtraDays, f => f.Random.Int(1, 90))
+              .Generate();
+        }
+    }
+}
