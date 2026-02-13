2046046669bbe568fd1a227d98428222842a90cd
daylon <daylonfellipe3001@outlook.com>
2026-02-13 19:15:28 +0000

    ADD - Added UpdateEmailTest on Patch WebApiTest

--- Tests/CommonTestUtilities/Requests/User/RequestUpdateUserNameJsonBuilder.cs
+++ Tests/CommonTestUtilities/Requests/User/RequestUpdateUserNameJsonBuilder.cs
@@ -5,10 +5,10 @@ namespace CommonTestUtilities.Requests.User
 {
     public class RequestUpdateUserNameJsonBuilder
     {
-        public static RequestUpdateUserNameJson Build()
+        public static RequestUpdateUserNameJson Build(Guid? id = null)
         {
             return new Faker<RequestUpdateUserNameJson>()
-                .RuleFor(u => u.Id, f => Guid.NewGuid())
+                .RuleFor(u => u.Id, f => id ?? Guid.NewGuid())
                 .RuleFor(u => u.FirstName, f => f.Name.FirstName())
                 .RuleFor(u => u.LastName, f => f.Name.LastName())
                 .Generate();
